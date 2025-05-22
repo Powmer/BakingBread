cee4f8b43e33655729baa59d4df5951c14a69954
Powmer <punchabunch3000@gmail.com>
2025-05-22 17:44:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template_string, request, redirect, flash
 
 app = Flask(__name__)
+app.secret_key = 'chave_secreta_simples'
+
+with open('index.html', 'r', encoding='utf-8') as f:
+    html_template = f.read()
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template_string(html_template)
 
-@app.route('/modules')
-def modules():
-    course_modules = [
-        "Introdução ao pão caseiro",
-        "Tipos de farinha e ingredientes",
-        "Fermentação e leveduras",
-        "Técnicas de amassar",
-        "Modelagem e fermentação final",
-        "Assando o pão perfeito",
-        "Receitas especiais: pão integral, ciabatta e mais"
-    ]
-    return render_template('modules.html', modules=course_modules)
+@app.route('/inscricao', methods=['POST'])
+def inscricao():
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    mensagem = request.form.get('mensagem')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+    if not nome or not email:
+        flash('Por favor, preencha nome e e-mail.')
+        return redirect('/')
+
+    print(f'Inscrição recebida: {nome}, {email}, {mensagem}')
+    flash('Inscrição enviada com sucesso!')
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
