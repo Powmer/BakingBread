247d17209321d6709e3b9cccf2feb89b3a4069a6
Powmer <punchabunch3000@gmail.com>
2025-05-22 18:02:07 -0300

    senha generica (app.py)

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
+import sqlite3
 from flask import Flask, render_template_string, request, redirect, flash
 
 app = Flask(__name__)
-app.secret_key = 'chave_secreta_simples'
+app.secret_key = 'Senhanadagenerica123'
 
 with open('index.html', 'r', encoding='utf-8') as f:
     html_template = f.read()
+
+DB_NAME = 'alunos.db'
+
+def criar_tabela():
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS inscricoes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL,
+            mensagem TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+criar_tabela()
 
 @app.route('/')
 def home():
@@ -20,9 +39,13 @@ def inscricao():
         flash('Por favor, preencha nome e e-mail.')
         return redirect('/')
 
-    print(f'Inscrição recebida: {nome}, {email}, {mensagem}')
-    flash('Inscrição enviada com sucesso!')
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('INSERT INTO inscricoes (nome, email, mensagem) VALUES (?, ?, ?)', (nome, email, mensagem))
+    conn.commit()
+    conn.close()
 
+    flash('Inscrição enviada com sucesso!')
     return redirect('/')
 
 if __name__ == '__main__':
